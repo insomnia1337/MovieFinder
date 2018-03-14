70641f273f8138be4d90b62ab28d81b5e07f4420
insomnia1337 <mateusz.bieda95@gmail.com>
2018-03-14 17:55:21 +0100

    add api KEY

--- src/Components/app-main/app-main.js
+++ src/Components/app-main/app-main.js
@@ -3,6 +3,19 @@ import Card from './../card/card';
 import Search from './../search/search';
 
 class AppMain extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            movieID: 157336
+        }
+    }
+    componentDidMount() {
+        fetch("https://api.themoviedb.org/3/movie/${movieID}?api_key=be379f00ab45c1359be1344dedeeabf8")
+          .then(res => res.json())
+          .then(json => this.setState({ data: json.results }));
+      }
+      
+//tutaj trzeba wyciagnac zmienne ktore beda nam potrzebne
     render() {
         return (
             <div className="overlay">
