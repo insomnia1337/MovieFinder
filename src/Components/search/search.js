7a661d6dc6faacaa791a47bbf75ab31d16453633
insomnia1337 <mateusz.bieda95@gmail.com>
2018-03-14 15:20:45 +0100

    Refactoring code

--- src/Components/search/search.js
+++ src/Components/search/search.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 class Search extends React.Component {
     render() {
-        return 
+        return (
+            <div className="container search-container">
+                <div className="row">
+                    <div className="col-12 col-sm-6 col-lg-5">
+                        <a href="#" title="Movie Finder"><img
+                            src="https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg"
+                            className="logo"
+                            alt="Movies"/></a>
+                    </div>
+                    <div className="col-12 col-sm-6 col-lg-7">
+                        <form className="searchbar">
+                            <input
+                                type="text"
+                                className="form-control searchbox__input"
+                                placeholder="Search movie..."/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
 
-
-        
     }
 }
 
