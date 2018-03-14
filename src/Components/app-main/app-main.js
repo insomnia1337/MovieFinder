55ec41d76d39dc3c42bb5ee5134c1cfcda25e2c5
insomnia1337 <mateusz.bieda95@gmail.com>
2018-03-14 12:26:27 +0100

    change styles structure

--- src/Components/app-main/app-main.js
+++ src/Components/app-main/app-main.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import './app-main.scss';
-import './../../styles/bootstrap.css'
 
 
 class AppMain extends React.Component {
     render() {
-        return(
-        <div className="container">
-        <div className="row">
-        <div className="col-sm-12">
-            <p>Jazda</p>
-        </div>
-        
-        </div>
-        </div>
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <p>Jazda</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ import { render } from 'react-dom';
 import './styles/app.scss'
 import AppMain from './Components/app-main/app-main';
 
+
 render(<AppMain />, document.getElementById('root'));
