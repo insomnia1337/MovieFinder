b760ada4c579e8a238934e5b823a8828cfacbd20
insomnia1337 <mateusz.bieda95@gmail.com>
2018-03-14 11:27:17 +0100

    move app.scss to styles folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './app.scss';
+import './styles/app.scss'
 import AppMain from './Components/app-main/app-main';
 
 render(<AppMain />, document.getElementById('root'));
